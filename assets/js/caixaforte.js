4b3ccf3f586f57e366a5b271b2cf517aa46889de
erico-senac <professorerico@pi.senac.br>
2026-04-07 21:47:02 -0300

    crud mais

--- assets/js/caixaforte.js
+++ assets/js/caixaforte.js
@@ -7,7 +7,26 @@ const data = document.querySelector('[name="data"]')
 const formaPagamento = document.querySelector('[name="formadepagamento"]')
 const observacao = document.querySelector('[name="textareaobservacao"]')
 
-let dados = [];
+let dados = localStorage.getItem('dados') !== null ? JSON.parse(localStorage.getItem('dados')) : [];
+
+const limpaDestino = () => {
+    document.querySelector('#despesa ul').innerHTML = '';
+    document.querySelector('#credito ul').innerHTML = '';
+}
+
+const inseriDetalhamento = (dados) => {
+    limpaDestino();
+    dados.forEach(dado => {
+        let destino = document.querySelector(`#${(dado.tipo).toLowerCase()} ul`);
+        let itemLista = document.createElement('li');
+        let apagar = document.createElement('button');
+        apagar.innerHTML = '<i class="fa-solid fa-trash-can"></i>'
+        apagar.addEventListener('click', () => apagarRegistro);
+        itemLista.innerText = `${dado.descricao} -> R$ ${dado.valor}`;
+        itemLista.appendChild(apagar);
+        destino.appendChild(itemLista);
+    })
+}
 
 btnSalvar.addEventListener('click', (e) => {
     e.preventDefault();
@@ -23,5 +42,7 @@ btnSalvar.addEventListener('click', (e) => {
 
     dados.push(dado);
     salvarDados(dados);
+    inseriDetalhamento(dados);
 });
     
+inseriDetalhamento(dados);
